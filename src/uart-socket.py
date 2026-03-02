beec26d95b38d886b1a2d176214bbcd7426857ee
Josh <eejl609@aucklanduni.ac.nz>
2026-03-03 09:50:27 +1300

    initial commit

---
+++ src/uart-socket.py
@@ -0,0 +1,31 @@
+import time
+
+import serial
+
+
+def init_pipe(
+    port: str = "/dev/ttyUSB0",
+    baud: int = 9600,
+    data_bits: int = 8,
+    stop_bits: int = 1,
+    parity: bool = None,
+    timeout: float = 10,
+) -> serial.Serial | None:
+    try:
+        Serial = serial.Serial(port, baud, data_bits, parity, stop_bits, timeout)
+        time.sleep(1)
+    except ValueError as e:
+        print(f"Value Error, problem here: {e} ")
+        return 0
+    except serial.SerialException as e:
+        print(f"Serial Exception!: {e}")
+        return 0
+    return Serial
+
+
+def one_shot_read(Serial: serial.Serial) -> bytes:
+    try:
+        byte_message = Serial.read_until()
+    except Serial.timeout():
+        print("TIMEOUT WHILE WAITING FOR RX")
+    return byte_message
